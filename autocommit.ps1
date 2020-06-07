b2df06c806ea332667f9f4ce7b3065cf3ac7f992
VioletHydra <mrG0bliN.tm@gmail.com>
2020-06-07 10:51:27 +0300

    Auto-Commit

---
+++ autocommit.ps1
@@ -0,0 +1,28 @@
+$br = "";
+
+Clear-Host
+Clear-History
+
+"=====  Last Commit  =====  "
+$log = git log --stat --graph -1 
+$br
+
+"=====  New Files  ====="  
+git status -s 
+$br
+
+"=====  Start Upload  ====="  
+git commit -am ' Auto-Commit' 
+git push 
+$br
+
+"=====  SUCCESS!  ====="  
+$br
+
+"Press any key to continue ... "
+Pause 
+if ($log) { Exit } else { Clear-Host };
+"=====  Nothing happened  =====" 
+"=====  Upload is Aborted!  =====" 
+Pause
+Exit
