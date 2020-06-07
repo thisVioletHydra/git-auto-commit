0f2c7dc2fd9a8c82d313f306003d1d7d16f8bf22
VioletHydra <mrG0bliN.tm@gmail.com>
2020-06-07 14:26:43 +0300

    ⭐ Auto-Commit 06/07/2020 14:26:43

--- autocommit.ps1
+++ autocommit.ps1
@@ -7,7 +7,7 @@ $gitLog = git log --stat --graph -1;
 Clear-Host;
 
 function _info {
-  param( [String] $str, $obj = "aasda" )
+  param( $str, $obj = "aasda" )
   Write-Host "[$time] [INFO]:" $str -ForegroundColor Cyan; $obj; $br;
 } 
 
