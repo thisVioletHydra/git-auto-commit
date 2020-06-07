279a404613d1a9a6d22f6afccc901417227c260e
VioletHydra <mrG0bliN.tm@gmail.com>
2020-06-07 15:01:09 +0300

    ⭐ Auto-Commit 06/07/2020 15:01:09

--- autocommit.ps1
+++ autocommit.ps1
@@ -1,39 +1,46 @@
 # Set-StrictMode ON
 Set-StrictMode -Version Latest
 
-$br = "";
-$time = Get-Date;
-$gitStatus = git status -s;
-$emptyGit = $gitStatus.Length  
-$gitLog = git log --stat --graph -1;
 
-Clear-Host;
 
-function _info([String]$str, [String]$str2 = $null) {
-  Write-Host "[$time] [INFO]:" $str -ForegroundColor Cyan; $str2; $br;
-} 
-
-function _done([String]$str) {
-  Write-Host "[$time] [DONE]:" $str -ForegroundColor Green;
-} 
-
-function _warn( [String]$str ) {
-  # Clear-Host;
-  Write-Host "[$time] [WARN]:" $str -ForegroundColor Yellow;
-} 
-
-_info "🙄 Last Commit" $gitLog   
-_info "🤔 New Files" $gitStatus 
-_info "debug " $emptyGit
-
-if ($emptyGit -or $null) {
-  _info "✈️ Uploading files"; git commit -am "⭐ Auto-Commit $time"; git push; $br;
-  _done("🟢 SUCCESS!"); $br;
+try {
+  $br = "";
+  $time = Get-Date;
+  $gitStatus = git status -s;
+  $emptyGit = $gitStatus.Length  
+  $gitLog = git log --stat --graph -1;
+  
+  Clear-Host;
+  
+  function _info([String]$str, [String]$str2 = $null) {
+    Write-Host "[$time] [INFO]:" $str -ForegroundColor Cyan; $str2; $br;
+  } 
+  
+  function _done([String]$str) {
+    Write-Host "[$time] [DONE]:" $str -ForegroundColor Green;
+  } 
+  
+  function _warn( [String]$str ) {
+    # Clear-Host;
+    Write-Host "[$time] [WARN]:" $str -ForegroundColor Yellow;
+  } 
+  
+  _info "🙄 Last Commit" $gitLog   
+  _info "🤔 New Files" $gitStatus 
+  _info "debug " $emptyGit
+  
+  if ($emptyGit -or $null) {
+    _info "✈️ Uploading files"; git commit -am "⭐ Auto-Commit $time"; git push; $br;
+    _done("🟢 SUCCESS!"); $br;
+  }
+  else { 
+    _warn("🔴 Nothing happened"); $br;
+  };
+  
+  #"debug: $state" 
+  Read-Host "Press any key to continue ...";
+  Exit;
 }
-else { 
-  _warn("🔴 Nothing happened"); $br;
-};
-
-#"debug: $state" 
-Read-Host "Press any key to continue ...";
-Exit;
+catch {
+  _warn("🔴 error"); $br;
+}
